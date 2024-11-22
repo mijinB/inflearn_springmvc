352f74516bb0ccd7c6eafc92df0df3998e5f66c6
Bmijin <bmj13465@jtts.co.kr>
2024-11-22 14:22:23 +0900

    HTTP 요청 파라미터 - JSON

--- src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
+++ src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
@@ -38,7 +38,7 @@ public class RequestBodyStringController {
     }
 
     @PostMapping("/request-body-string-v3")
-    public HttpEntity<String> requestBodyStringV3(HttpEntity<String> httpEntity) throws IOException {
+    public HttpEntity<String> requestBodyStringV3(HttpEntity<String> httpEntity) {
         String messageBody = httpEntity.getBody();
         log.info("messageBody={}", messageBody);
 
@@ -47,7 +47,7 @@ public class RequestBodyStringController {
 
     @ResponseBody
     @PostMapping("/request-body-string-v4")
-    public String requestBodyStringV4(@RequestBody String messageBody) throws IOException {
+    public String requestBodyStringV4(@RequestBody String messageBody) {
         log.info("messageBody={}", messageBody);
 
         return "ok";
